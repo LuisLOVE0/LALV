40c89df6fec179bbde20bfeebffed061b1088332
LuisLOVE0 <llopezv2200@alumno.ipn.mx>
2024-04-07 00:06:58 -0600

    prueba form 2

--- Menu de los formularios/Formularios/formulas.js
+++ Menu de los formularios/Formularios/formulas.js
@@ -2,9 +2,17 @@ document.getElementById("formulariodelbanco").addEventListener("submit", functio
     event.preventDefault();
     
     var Capitalinicial = parseFloat(document.getElementById("Capitalinicial").value);
-    
     var monthlyProfit = Capitalinicial * 0.02;
     
     document.getElementById("Ganancia").innerText = "$" + monthlyProfit.toFixed(2);
     document.getElementById("resultado").style.display = "block";
-})
+
+    if (!validarNumero(Capitalinicial)) {
+        alert("Por favor, ingresa solo numeros.");
+        event.preventDefault();
+    }
+});
+
+function validarNumero(input) {
+    return !isNaN(input);
+}
